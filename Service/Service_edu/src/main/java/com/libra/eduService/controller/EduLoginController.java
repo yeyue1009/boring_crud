fd5acaef13a84ac0f504706cec53a49969093215
yeyue <yeyue1009@gmail.com>
2021-03-01 16:12:15 +0800

    简易登录功能改造

---
+++ Service/Service_edu/src/main/java/com/libra/eduService/controller/EduLoginController.java
@@ -0,0 +1,23 @@
+package com.libra.eduService.controller;
+
+
+import com.libra.eduService.common.utils.R;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/eduService/user")
+@CrossOrigin
+public class EduLoginController {
+
+    //login
+    @PostMapping("login")
+    public R login() {
+        return R.ok().data("token","admin");
+    }
+
+    @GetMapping("info")
+    public R info() {
+        return R.ok().data("roles","[admin]").data("name","admin").data("avatar","https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+    }
+}
